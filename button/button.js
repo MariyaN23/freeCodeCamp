17a8b878be9f68ac26ed958f4dbab366e5c436ab
MariyaN23 <sellaite505@gmail.com>
2023-08-14 19:41:39 +0300

    button

--- button/button.js
+++
@@ -1,8 +0,0 @@
-const button3 = document.querySelector('#button3')
-const output = document.querySelector('#output')
-button3.addEventListener('click', ()=> {
-    const boxForMessage = document.createElement('p')
-    const random = Math.round(Math.random()*10)
-    boxForMessage.textContent = 'Случайное число:'+random
-    output.appendChild(boxForMessage)
-})
