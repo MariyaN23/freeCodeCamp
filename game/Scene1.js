a7c9bcec87f25263c1eb0e5e755a92a19ba76aee
MariyaN23 <sellaite505@gmail.com>
2023-11-30 16:46:42 +0300

    game

---
+++ game/Scene1.js
@@ -0,0 +1,28 @@
+class Scene1 extends Phaser.Scene {
+    constructor() {
+        super("bootGame")
+    }
+    preload(){
+        this.load.image("background", "assets/background.png")
+        this.load.spritesheet("ship", "assets/ship.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        })
+        this.load.spritesheet("ship2", "assets/ship2.png", {
+            frameWidth: 32,
+            frameHeight: 16
+        })
+        this.load.spritesheet("ship3", "assets/ship3.png", {
+            frameWidth: 32,
+            frameHeight: 32
+        })
+        this.load.spritesheet("explosion", "assets/explosion.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        })
+    }
+    create() {
+        this.add.text(20, 20, "Loading game...")
+        this.scene.start("playGame")
+    }
+}
---
+++ game/game.js
@@ -0,0 +1,9 @@
+let config = {
+    width: 256,
+    height: 272,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2],
+    pixelArt: true,
+}
+
+let game = new Phaser.Game(config);
